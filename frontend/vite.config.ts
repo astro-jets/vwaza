4aa06865dba739cc39c3beba6bfb5f6b996022b0
astro-jets <mphatsomtogolo30@gmail.com>
2026-08-02 09:32:33 +0200

    Fixed SSR issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { vercelPreset } from "@vercel/react-router/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), vercelPreset()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
