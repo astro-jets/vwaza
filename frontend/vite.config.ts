bb2d9f6e24a7c40e79a607d77b92be4e58002e8f
astro-jets <mphatsomtogolo30@gmail.com>
2026-08-02 22:37:27 +0200

    fixed vercel vite issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    // Regex matches 'gsap', 'gsap/all', 'gsap/CustomEase', etc.
+    noExternal: [/gsap/],
+  },
 });
